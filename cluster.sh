50806fc0cd66d369bd555d18c0bbaeb1db707a2b
Charles-Edouard Brétéché <charled.breteche@gmail.com>
2022-02-07 23:12:34 +0100

    Add helper functions

--- cluster.sh
+++ cluster.sh
@@ -16,6 +16,28 @@ log(){
   echo "---------------------------------------------------------------------------------------"
 }
 
+wait_ready(){
+  local NAME=${1:-pods}
+  local TIMEOUT=${2:-5m}
+  local SELECTOR=${3:---all}
+
+  log "WAIT $NAME ($TIMEOUT) ..."
+
+  kubectl wait -A --timeout=$TIMEOUT --for=condition=ready $NAME $SELECTOR
+}
+
+wait_pods_ready(){
+  local TIMEOUT=${1:-5m}
+
+  wait_ready pods $TIMEOUT --field-selector=status.phase!=Succeeded
+}
+
+wait_nodes_ready(){
+  local TIMEOUT=${1:-5m}
+
+  wait_ready nodes $TIMEOUT
+}
+
 network(){
   local NAME=${1:-kind}
 
