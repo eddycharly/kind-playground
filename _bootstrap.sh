a4ccd473f45571569d6951a36e1778e2ee70ae22
Charles-Edouard Brétéché <charled.breteche@gmail.com>
2022-03-04 14:23:44 +0100

    Add gitops support

--- _bootstrap.sh
+++ _bootstrap.sh
@@ -1,12 +1,81 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# VARIABLES
+
+KUBE_PROMETHEUS_STACK=false
+GITOPS=false
+
+# FUNCTIONS
+
+log(){
+  echo "---------------------------------------------------------------------------------------"
+  echo $1
+  echo "---------------------------------------------------------------------------------------"
+}
+
+verifySupported(){
+  if ! type "kubectl" > /dev/null 2>&1; then
+    echo "kubectl is required"
+    exit 1
+  fi
+
+  if ! type "curl" > /dev/null 2>&1; then
+    echo "curl is required"
+    exit 1
+  fi
+
+  if ! type "helm" > /dev/null 2>&1; then
+    echo "helm is required"
+    exit 1
+  fi
+}
+
+fail_trap() {
+  local RESULT=$?
+  log "FAILED WITH RESULT $RESULT !!!"
+  exit $RESULT
+}
+
+# RUN
+
+trap "fail_trap" EXIT
 
 set -e
 
-# RUN
+set -u
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    '--gitops')
+      GITOPS=true
+      ;;
+    '--kube-prometheus-stack')
+      KUBE_PROMETHEUS_STACK=true
+      ;;
+    *)
+      echo "ERROR: Unknown option $1"
+      help
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+set +u
+
+verifySupported
 
 ./cluster.sh
 ./keycloak.sh
 ./argocd.sh
 ./gitea.sh
-./kube-prometheus-stack.sh
-./argocd-applications.sh
+
+if [ "$KUBE_PROMETHEUS_STACK" == "true" ]; then
+    ./kube-prometheus-stack.sh
+fi
+
+if [ "$GITOPS" == "true" ]; then
+    ./gitops.sh
+else
+    ./argocd-applications.sh
+fi
---
+++ gitops.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+
+set -e
+
+# CONSTANTS
+
+readonly DNSMASQ_DOMAIN=kind.cluster
+
+# FUNCTIONS
+
+log(){
+  echo "---------------------------------------------------------------------------------------"
+  echo $1
+  echo "---------------------------------------------------------------------------------------"
+}
+
+get_subnet(){
+  docker network inspect -f '{{(index .IPAM.Config 0).Subnet}}' $1
+}
+
+subnet_to_ip(){
+  echo $1 | sed "s@0.0/16@$2@"
+}
+
+cleanup(){
+  log "CLEANUP ..."
+
+  rm -rf .gitops
+}
+
+init(){
+  log "INIT ..."
+
+  mkdir .gitops
+  git init .gitops
+  git -C .gitops remote add origin http://gitea_admin:admin@gitea.kind.cluster/gitea_admin/gitops.git
+  git -C .gitops fetch --all || true
+  git -C .gitops pull origin master || true
+}
+
+install(){
+  log "INSTALL ..."
+
+  cp -r helm/ .gitops/
+}
+
+push(){
+  log "PUSH ..."
+
+  git -C .gitops add .
+  git -C .gitops commit -m "gitops" --allow-empty
+  git -C .gitops push -u origin master
+}
+
+bootstrap(){
+  log "BOOTSTRAP ..."
+
+  local KIND_SUBNET=$(get_subnet kind)
+  local METALLB_START=$(subnet_to_ip $KIND_SUBNET 255.200)
+  local METALLB_END=$(subnet_to_ip $KIND_SUBNET 255.250)
+
+  kubectl apply -n argocd -f - <<EOF
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: gitops
+  finalizers:
+    - resources-finalizer.argocd.argoproj.io
+spec:
+  project: default
+  source:
+    repoURL: http://gitea.kind.cluster/gitea_admin/gitops
+    path: helm/gitops
+    targetRevision: HEAD
+    helm:
+      values: |
+        prometheus:
+          operator:
+            enabled: false
+        dns:
+          private: $DNSMASQ_DOMAIN
+        metallb:
+          start: $METALLB_START
+          end: $METALLB_END
+  destination:
+    server: https://kubernetes.default.svc
+    namespace: argocd
+  revisionHistoryLimit: 3
+  syncPolicy:
+    syncOptions:
+      - ApplyOutOfSyncOnly=true
+      - CreateNamespace=true
+      - FailOnSharedResource=true
+      - PruneLast=true
+    automated:
+      prune: true
+      selfHeal: true
+EOF
+}
+
+# RUN
+
+cleanup
+init
+install
+push
+bootstrap
+
+# DONE
+
+log "GITOPS READY !"
